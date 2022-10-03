a69eb460ac7b18ec0456b700e59e53efa2c4592b
Corbin McCalister <mccalisterc2003@gmail.com>
2022-10-02 22:04:00 -0500

    Initial Commit

---
+++ Assets/Scripts/FallScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallScript : MonoBehaviour
+{
+    MeshRenderer renderer;
+    Rigidbody rigidBody;
+    [SerializeField] float time = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+        rigidBody = GetComponent<Rigidbody>();
+
+        renderer.enabled = false;
+        rigidBody.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time >= time)
+        {
+            renderer.enabled = true;
+            rigidBody.useGravity = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Scorer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorer : MonoBehaviour
+{
+    int count = 0;
+   private void OnCollisionEnter(Collision other) 
+   {
+       if(other.gameObject.tag != "Hit")
+       {
+           count++;
+           Debug.Log("You've bumped into an obstacle this many times: " + count);
+       }
+   }
+}
